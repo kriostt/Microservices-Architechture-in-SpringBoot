a95c764e67ee979b1b24ccf1a1347701d23d7bd9
kriostt <129346100+kriostt@users.noreply.github.com>
2024-03-29 22:49:21 -0600

    Initial BookService and OrderService creation

---
+++ BookService/src/test/java/CPRO2501/BookService/BookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package CPRO2501.BookService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
