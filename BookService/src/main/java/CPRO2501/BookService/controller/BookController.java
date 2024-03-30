7b00361729e128d34d1f20fa56c5f3508472cc39
kriostt <129346100+kriostt@users.noreply.github.com>
2024-03-29 22:50:29 -0600

    Added BookController

---
+++ BookService/src/main/java/CPRO2501/BookService/controller/BookController.java
@@ -0,0 +1,47 @@
+package CPRO2501.BookService.controller;
+
+import CPRO2501.BookService.entity.Book;
+import CPRO2501.BookService.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+// handle HTTP requests related to Book entities
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    // automatic injection of BookService instance
+    @Autowired
+    private BookService bookService;
+
+    // handle HTTP POST request to add a new book
+    @PostMapping("/add")
+    public String addBook(@RequestBody Book book) {
+        return bookService.addBook(book);
+    }
+
+    // handle HTTP GET request to retrieve all books
+    @GetMapping("/all")
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    // handle HTTP GET request to retrieve a book by ID
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable long id) {
+        return bookService.getBookById(id);
+    }
+
+    // handle HTTP PUT request to update existing book
+    @PutMapping("/update/{id}")
+    public String updateBook(@PathVariable long id, @RequestBody Book book) {
+        return bookService.updateBook(id, book);
+    }
+
+    // handle HTTP DELETE request to delete a book
+    @DeleteMapping("/delete/{id}")
+    public String deleteBook(@PathVariable long id) {
+        return bookService.deleteBook(id);
+    }
+}
